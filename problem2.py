7d478c5f846bff4f5b17eb47e8971885bca9c0ef
Zhamal722 <114822767+Zhamal722@users.noreply.github.com>
2022-10-03 11:00:16 +0200

    first commit

---
+++ problem2.py
@@ -0,0 +1,18 @@
+a = 6 
+b = 4
+c = 8 
+
+print(a > b, c > a)
+
+d = 7 % 3 * 4.8 
+
+print(d)
+
+e = 32
+f = 34 - 2
+print(e == f)
+
+k = 24
+l = 36
+
+print(k != l)
---
+++ problem9.py
@@ -0,0 +1,7 @@
+a = 1996
+b = 2022
+c = (b - a) + 2
+print(c)
+
+d = (b - a) - 2
+print(d)
